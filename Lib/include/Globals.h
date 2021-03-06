053e1d0adebe61fa851a6c18745e4ea79758f7e8
binh <dangbinh4869@gmail.com>
2021-03-06 22:51:10 +0700

    + added get int and line utilities

--- Lib/include/Globals.h
+++ Lib/include/Globals.h
@@ -1,4 +1,6 @@
 #pragma once
+#include <iostream>
+#include <string>
 
 #define CLS_SCR system("cls")
 #define QUIT system("exit")
@@ -8,17 +10,42 @@ constexpr int COL = 3;
 constexpr int BOARD_SIZE = ROW * COL;
 constexpr int NAME_SIZE = 30;
 
-static int GetInt(int min, int max) {
-    int input;
-    int temp, check = scanf_s("%d%c", &input);
-    fflush(stdin);
-    while (check != 2) {
-        if (input < min || input > max) {
-            while ((temp = getchar()) != EOF && temp != '\n');
-            printf("invalid move: ");
-            check = scanf_s("%d%c", &input);
-            fflush(stdin);
+namespace Utils {
+    inline bool IsInt(const std::string& s) {
+        std::string::const_iterator it = s.begin();
+        while (it != s.end() && std::isdigit(*it)) ++it;
+        return !s.empty() && it == s.end();
+    }
+
+    /// <summary>
+    /// get and validate integer value from input
+    /// </summary>
+    /// <param name="min">min value</param>
+    /// <param name="max">max value</param>
+    /// <param name="message">message display</param>
+    /// <returns>valid integer value</returns>
+    static int GetInt(int min, int max, const std::string& message = "") {
+        while (true) {
+            std::cout << message;
+            std::string temp;
+            std::getline(std::cin, temp);
+            if (!temp.empty() && (IsInt(temp) || (IsInt(temp.substr(1, temp.length() - 1)) && temp[0] == '-'))) {
+                const int result = std::stoi(temp, nullptr, 10);
+                if (result > max || result < min) {
+                    std::cout << "PLEASE ENTER AN INTEGER IN THE RANGE FROM " << min << " TO " << max << std::endl;
+                    continue;
+                }
+                return result;
+            }
+            else {
+                std::cout << "PLEASE ENTER AN INTEGER" << std::endl;
+            }
         }
     }
-    return input;
-}
+
+    static void GetLine(char* str, size_t size, const std::string& message = "") {
+        std::cout << message;
+        std::cin.get(str, size);
+        std::cin.ignore();
+    }
+};
