9936f06f5137d9dfc603f38274ac528bfb7fb9af
binh.buidang <dangbinh4869@gmail.com>
2021-03-18 10:49:50 +0700

    + Dectect memory leak

--- Cpp_Practice/Lib/include/Globals.h
+++ Cpp_Practice/Lib/include/Globals.h
@@ -5,6 +5,19 @@
 #include <ctime>
 #include <random>
 
+// for memory leak detection
+#define _CRTDBG_MAP_ALLOC
+#include <cstdlib>
+#include <crtdbg.h>
+
+#ifdef _DEBUG
+#define DBG_NEW new ( _NORMAL_BLOCK , __FILE__ , __LINE__ )
+// Replace _NORMAL_BLOCK with _CLIENT_BLOCK if you want the
+// allocations to be of _CLIENT_BLOCK type
+#else
+#define DBG_NEW new
+#endif
+
 #define CLS_SCR system("cls")
 #define QUIT system("exit")
 
