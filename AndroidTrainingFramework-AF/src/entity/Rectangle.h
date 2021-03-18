5b897e38cb86cce480e40b32d65dcf55f1328b16
binh.buidang <dangbinh4869@gmail.com>
2021-03-18 09:03:50 +0700

    + Added some basic entities

--- AndroidTrainingFramework-AF/src/entity/Rectangle.h
+++ AndroidTrainingFramework-AF/src/entity/Rectangle.h
@@ -1,7 +1,21 @@
 #pragma once
 #include "Object.h"
 
-class Rectangle : public Object
-{
-};
+namespace Entity {
+    class Rectangle : public Object
+    {
+    public:
+        Rectangle();
+        Rectangle(const Vector2& p_position, float p_width, float p_height);
+        void Render() override;
+        void Update(float frameTime) override;
+        bool Contains(const Vector2& p_coord) override;
+        void SetPosition(const Vector2& p_position) override;
+        int GetWidth() const;
+        int GetHeight() const;
+    private:
+        int m_width;
+        int m_height;
+    };
+}
 
