7c8e79d8bbf445dfe612081c6edade7b9462074d
binh.buidang <dangbinh4869@gmail.com>
2021-03-18 13:51:39 +0700

    + Added gameplay for tictactoe.

---
+++ Cpp_Practice/Lib/include/tictactoe/Game.h
@@ -0,0 +1,26 @@
+#pragma once
+#include <vector>
+
+class Game {
+public:
+    Game();
+    ~Game();
+    int Start();
+    static void DisplayBoard(std::vector<char>& p_pieces);
+    void Play();
+    void Replay();
+    void TurnPlay(char piece);
+    bool Save();
+
+private:
+    char* player1;
+    char* player2;
+    std::vector<char> pieces;
+    bool running;
+    int turn;
+    std::vector<std::vector<char>> steps;
+
+    bool CheckWin();
+    // void AddStep(const std::vector<char>& step);
+};
+
